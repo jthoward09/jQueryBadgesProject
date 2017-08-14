3fa8df2a2071d3da4d20c291a03e0f10fc1882e3
James Howard <jthoward09@gmail.com>
2017-08-14 01:41:33 -0400

    Adds js code for badge project

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
+  var $badgesDiv = $('#badges');
+  $.ajax( {
+    url: 'https://www.codeschool.com/users/jthoward09.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var badgeHTML = '';
+      // Loop through each object in the course completed array
+      $.each(response.courses.completed, function(index, item) {
+            badgeHTML += "<div class='course'>" +
+                            "<h3>" + item.title + "</h3>" +
+                            "<img src='" + item.badge + "' alt='"  + item.title + "'>" +
+                            "<a target='_blank' class='btn btn-primary' href='" + item.url + "'>See Courses</a>" +
+                          "</div>"
 
-  // your code will go here
+      });
+      $("#badges").append(badgeHTML);
+    },
+    error: function(request, errorType, errorMessage) {
+      alert(errorMessage)
+    }
+  });
 
 });
